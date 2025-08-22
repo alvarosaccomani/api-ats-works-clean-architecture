2801b7007f7612dd08a7267f9634323847d5904c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-22 19:00:09 -0300

    i add item.repository.ts into item folder into folder domain to project

---
+++ src/domain/item/item.repository.ts
@@ -0,0 +1,10 @@
+import { ItemEntity } from "./item.entity";
+
+export interface ItemRepository {
+    getItems(): Promise<ItemEntity[] | null>;
+    findItemById(itm_uuid: string): Promise<ItemEntity | null>;
+    createItem(item: ItemEntity): Promise<ItemEntity | null>;
+    updateItem(itm_uuid: string, item: ItemEntity): Promise<ItemEntity | null>;
+    deleteItem(itm_uuid: string): Promise<ItemEntity | null>;
+    findItemByName(itm_name: string, excludeUuid?: string | null): Promise<ItemEntity | null>;
+}
