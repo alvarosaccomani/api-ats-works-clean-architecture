aa694dcd1159aa389bd761240fcdd4519b59c862
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-22 18:59:25 -0300

    i add item.value.ts into item folder into folder domain to project

---
+++ src/domain/item/item.value.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from "uuid";
+import moment from 'moment';
+import { ItemEntity } from "./item.entity";
+
+export class ItemValue implements ItemEntity {
+    itm_uuid: string;
+    itm_name: string;
+    itm_description: string;
+    itm_createdat: Date;
+    itm_updatedat: Date;
+    
+    constructor({
+            itm_uuid,
+            itm_name,
+            itm_description,
+            itm_createdat,
+            itm_updatedat
+        }:{ 
+            itm_uuid: string,
+            itm_name: string,
+            itm_description: string,
+            itm_createdat: Date,
+            itm_updatedat: Date
+        }) {
+        this.itm_uuid = uuid();
+        this.itm_name = itm_name;
+        this.itm_description = itm_description;
+        this.itm_createdat = itm_createdat ?? moment().toDate();
+        this.itm_updatedat = itm_updatedat ?? moment().toDate();
+    }
+}
