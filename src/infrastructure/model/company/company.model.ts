e39146d67e8e3b1ba9e2d2bd39eef468197bfe81
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-22 19:06:09 -0300

    i add company.model.ts into company folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/company/company.model.ts
@@ -0,0 +1,50 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { CompanyEntity } from "../../../domain/company/company.entity";
+
+export class SequelizeCompany extends Model<CompanyEntity, Omit<CompanyEntity, 'id'>> {
+  declare cmp_uuid: string;
+  declare cmp_name: string;
+  declare cmp_address: string;
+  declare cmp_phone: string;
+  declare cmp_email: string;
+  declare cmp_createdat: Date;
+  declare cmp_updatedat: Date;
+}
+
+SequelizeCompany.init({
+  cmp_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true/*,
+    autoIncrement: true*/
+  },
+  cmp_name: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_address: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_phone: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_email: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  cmp_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  timestamps: false,
+  tableName: 'cmp_companies'
+});
+SequelizeCompany.sync();
