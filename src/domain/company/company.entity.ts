21c32296ead061943538c01d8b1031a39bcc465d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-22 19:04:58 -0300

    i add company.entity.ts into company folder into folder domain to project

---
+++ src/domain/company/company.entity.ts
@@ -0,0 +1,9 @@
+export interface CompanyEntity {
+    cmp_uuid: string,
+    cmp_name: string,
+    cmp_address: string,
+    cmp_phone: string,
+    cmp_email: string,
+    cmp_createdat: Date,
+    cmp_updatedat: Date
+}
