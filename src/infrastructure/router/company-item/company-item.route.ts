2cd99d8dfcc79d87f2feefbce9a8906eceb24d5e
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-22 21:18:34 -0300

    i add company-item.route.ts into company-item folder into folder router into folder infrastructure to project

---
+++ src/infrastructure/router/company-item/company-item.route.ts
@@ -0,0 +1,33 @@
+import { Express } from "express";
+import { SequelizeRepository } from "../../repository/company-item/sequelize-company-item.repository";
+import { CompanyItemUseCase } from "../../../application/company-item/company-item-use-case";
+import { CompanyItemController } from "../../controller/company-item/company-item.controller";
+import SocketAdapter from "../../services/socketAdapter";
+
+function configureCompanyItemRoutes(app: Express, socketAdapter: SocketAdapter) {
+    /*
+    *   Iniciar repository
+    */
+    
+    const sequelizeCompanyItemRepository = new SequelizeRepository();
+    
+    /*
+    *   Iniciar casos de uso
+    */
+    
+    const companyItemUseCase = new CompanyItemUseCase(sequelizeCompanyItemRepository);
+    
+    /*
+    *   Iniciar controller
+    */
+    
+    const companyItemCtrl = new CompanyItemController(companyItemUseCase, socketAdapter);
+    
+    app.get(`/${process.env.BASE_URL_API}/company-items/:filter?/:page?/:perPage?`, companyItemCtrl.getAllCtrl);
+    app.get(`/${process.env.BASE_URL_API}/company-item/:cmp_uuid/:itm_uuid/:cmpitm_uuid`, companyItemCtrl.getCtrl);
+    app.post(`/${process.env.BASE_URL_API}/company-item`, companyItemCtrl.insertCtrl);
+    app.put(`/${process.env.BASE_URL_API}/company-item/:cmp_uuid/:itm_uuid/:cmpitm_uuid`, companyItemCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/company-item/:cmp_uuid/:itm_uuid/:cmpitm_uuid`, companyItemCtrl.deleteCtrl);
+}
+
+export default configureCompanyItemRoutes;
