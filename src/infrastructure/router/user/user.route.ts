35b0ec30bae70bcd44f377f9d0d0bce47cafe62d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-23 19:26:37 -0300

    i add user.route.ts into user folder into folder router into folder infrastructure to project

---
+++ src/infrastructure/router/user/user.route.ts
@@ -0,0 +1,38 @@
+import { Express } from "express";
+import { SequelizeRepository } from "../../repository/user/sequelize-user.repository";
+import { UserUseCase } from "../../../application/user/user-use-case";
+import { UserController } from "../../../infrastructure/controller/user/user.controller";
+import SocketAdapter from "../../services/socketAdapter";
+
+function configureUserRoutes(app: Express, socketAdapter: SocketAdapter) {
+    /*
+    *   Iniciar repository
+    */
+    
+    const sequelizeUserRepository = new SequelizeRepository();
+    
+    /*
+    *   Iniciar casos de uso
+    */
+    
+    const userUseCase = new UserUseCase(sequelizeUserRepository);
+    
+    /*
+    *   Iniciar controller
+    */
+    
+    const userCtrl = new UserController(userUseCase, socketAdapter);
+    
+    app.get(`/${process.env.BASE_URL_API}/user`, userCtrl.getAllCtrl);
+    app.get(`/${process.env.BASE_URL_API}/user/:usr_uuid`, userCtrl.getCtrl);
+    app.post(`/${process.env.BASE_URL_API}/user`, userCtrl.insertCtrl);
+    app.put(`/${process.env.BASE_URL_API}/user/:usr_uuid`, userCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/user/:usr_uuid`, userCtrl.deleteCtrl);
+    app.post(`/${process.env.BASE_URL_API}/login`, userCtrl.loginCtrl);
+    app.post(`/${process.env.BASE_URL_API}/register`, userCtrl.saveCtrl);
+    app.post(`/${process.env.BASE_URL_API}/confirm-account`, userCtrl.confirmCtrl);
+    app.post(`/${process.env.BASE_URL_API}/forgot-password`, userCtrl.forgotCtrl);
+    app.post(`/${process.env.BASE_URL_API}/reset-password`, userCtrl.resetCtrl);
+}
+
+export default configureUserRoutes;
