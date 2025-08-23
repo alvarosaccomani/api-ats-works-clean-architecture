8a1070935bba12fb41784553e03acf1086816517
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-22 21:16:01 -0300

    i add company-item.value.ts into company-item folder into folder domain to project

---
+++ src/domain/company-item/company-item.value.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from "uuid";
+import moment from 'moment';
+import { CompanyItemEntity } from "./company-item.entity";
+
+export class CompanyItemValue implements CompanyItemEntity {
+    cmp_uuid: string;
+    itm_uuid: string;
+    cmpitm_uuid: string;
+    cmpitm_createdat: Date;
+    cmpitm_updatedat: Date;
+    
+    constructor({
+            cmp_uuid,
+            itm_uuid,
+            cmpitm_uuid,
+            cmpitm_createdat,
+            cmpitm_updatedat
+        }:{ 
+            cmp_uuid: string,
+            itm_uuid: string,
+            cmpitm_uuid: string,
+            cmpitm_createdat: Date,
+            cmpitm_updatedat: Date
+        }) {
+        this.cmp_uuid = cmp_uuid;
+        this.itm_uuid = itm_uuid;
+        this.cmpitm_uuid = uuid();
+        this.cmpitm_createdat = cmpitm_createdat ?? moment().toDate();
+        this.cmpitm_updatedat = cmpitm_updatedat ?? moment().toDate();
+    }
+}
