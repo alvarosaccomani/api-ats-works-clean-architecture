0ee7c8c0a8008e5b233d80d1fcb38a27779e9b77
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-24 20:02:03 -0300

    i add user-rol-company.entity.ts into user-rol-company folder into folder domain to project

---
+++ src/domain/user-rol-company/user-rol-company.entity.ts
@@ -0,0 +1,9 @@
+export interface UserRolCompanyEntity {
+    cmp_uuid: string,
+    usrrolcmp_uuid: string,
+    usr_uuid: string,
+    rol_uuid: string,
+    usrrolcmp_createdat: Date,
+    usrrolcmp_updatedat: Date
+
+}
