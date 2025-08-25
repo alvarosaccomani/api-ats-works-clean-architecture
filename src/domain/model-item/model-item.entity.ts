5391a1c611deb77183c4ada99ff53da447b7d8f1
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-25 18:13:18 -0300

    i add model-item.entity.ts into model-item folder into folder domain to project

---
+++ src/domain/model-item/model-item.entity.ts
@@ -0,0 +1,11 @@
+export interface ModelItemEntity {
+    cmp_uuid: string,
+    itm_uuid: string,
+    cmpitm_uuid: string,
+    mitm_uuid: string,	
+	mitm_name: string,
+	mitm_description: string,
+	mitm_active: boolean,
+	mitm_createdat: Date,
+    mitm_updatedat: Date
+}
