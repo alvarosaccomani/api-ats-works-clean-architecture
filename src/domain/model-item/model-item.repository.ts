5795e5282c5d52f9bd293258d17df2417347b685
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-25 18:16:10 -0300

    i add model-item.repository.ts into model-item folder into folder domain to project

---
+++ src/domain/model-item/model-item.repository.ts
@@ -0,0 +1,10 @@
+import { ModelItemEntity } from "./model-item.entity";
+
+export interface ModelItemRepository {
+    getModelItems(): Promise<ModelItemEntity[] | null>;
+    findModelItemById(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string): Promise<ModelItemEntity | null>;
+    createModelItem(modelItem: ModelItemEntity): Promise<ModelItemEntity | null>;
+    updateModelItem(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string, modelItem: ModelItemEntity): Promise<ModelItemEntity | null>;
+    deleteModelItem(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string): Promise<ModelItemEntity | null>;
+    existModelItem(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string): Promise<ModelItemEntity | null>;
+}
