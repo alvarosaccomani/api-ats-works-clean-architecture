45ab4e1e4872df004d503510560ce111b4dd8c93
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-25 18:17:27 -0300

    i add model-item.model.ts into model-item folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/model-item/model-item.model.ts
@@ -0,0 +1,59 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { ModelItemEntity } from "../../../domain/model-item/model-item.entity";
+
+export class SequelizeModelItem extends Model<ModelItemEntity, Omit<ModelItemEntity, 'id'>> {
+  declare cmp_uuid: string;
+  declare itm_uuid: string;
+  declare cmpitm_uuid: string;
+  declare mitm_uuid: string;
+	declare mitm_name: string;
+	declare mitm_description: string;
+	declare mitm_active: boolean;
+  declare mitm_createdat: Date;
+  declare mitm_updatedat: Date;
+}
+
+SequelizeModelItem.init({
+  cmp_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true
+  },
+  itm_uuid: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+  cmpitm_uuid: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+  mitm_uuid: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+  mitm_name: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+	mitm_description: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+	mitm_active: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true
+  },
+  mitm_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  mitm_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  timestamps: false,
+  tableName: 'mitm_modelsitems'
+});
+SequelizeModelItem.sync();
