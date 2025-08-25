b90b7bd5201089bdae29437c2817debdbbcaf606
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-24 22:29:51 -0300

    i add data-type.model.ts into data-type folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/data-type/data-type.model.ts
@@ -0,0 +1,51 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { DataTypeEntity } from "../../../domain/data-type/data-type.entity";
+
+export class SequelizeDataType extends Model<DataTypeEntity/*, Omit<DataTypeEntity, 'id'>*/> {
+  declare dtp_uuid: string;
+  declare dtp_cod: string;
+  declare dtp_name: string;
+  declare dtp_description: string;
+  declare dtp_active: boolean;
+  declare dtp_createdat: Date;
+  declare dtp_updatedat: Date;
+}
+
+SequelizeDataType.init({
+  dtp_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true/*,
+    autoIncrement: true*/
+  }, 
+  dtp_cod: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  dtp_name: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  dtp_description: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  dtp_active: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  },
+  dtp_createdat: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  dtp_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+}, {
+  sequelize,
+  timestamps: false,
+  tableName: 'dtp_datatypes'
+});
+
+SequelizeDataType.sync();
