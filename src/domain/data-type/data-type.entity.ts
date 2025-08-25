b3a7c32fa70ddb174a6bfa3dcc502f966cf05a5a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-24 22:14:06 -0300

    i add data-type.entity.ts into data-type folder into folder domain to project

---
+++ src/domain/data-type/data-type.entity.ts
@@ -0,0 +1,9 @@
+export interface DataTypeEntity {
+    dtp_uuid: string,
+    dtp_cod: string,
+    dtp_name: string,
+    dtp_description: string,
+    dtp_active: boolean,
+    dtp_createdat: Date,
+    dtp_updatedat: Date
+}
