7fe791b81ebf3b1a787712b89aa7e0e861878f31
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-27 18:47:21 -0300

    i add address.entity.ts into address folder into folder domain to project

---
+++ src/domain/address/address.entity.ts
@@ -0,0 +1,11 @@
+export interface AddressEntity {
+    cmp_uuid: string,
+    adr_uuid: string,
+    cus_uuid: string,    
+    adr_address: string,
+    adr_city: string,
+    adr_province: string,    
+    adr_postalcode: string,    
+    adr_createdat: Date,
+    adr_updatedat: Date
+}
