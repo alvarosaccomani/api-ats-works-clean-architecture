92a2806e577b0460255cbdffa1874ee1c253cab8
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-27 18:42:59 -0300

    i add customer.model.ts into customer folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/customer/customer.model.ts
@@ -0,0 +1,56 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { CustomerEntity } from "../../../domain/customer/customer.entity";
+
+export class SequelizeCustomer extends Model<CustomerEntity, Omit<CustomerEntity, 'id'>> {
+  declare cmp_uuid: string;
+  declare cus_uuid: string;
+  declare cus_fullname: string;
+  declare cus_email: string;
+  declare cus_phone: string;    
+  declare usr_uuid: string;
+  declare cus_createdat: Date;
+  declare cus_updatedat: Date;
+}
+
+SequelizeCustomer.init({
+  cmp_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true/*,
+    autoIncrement: true*/
+  },
+  cus_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true/*,
+    autoIncrement: true*/
+  },
+  cus_fullname: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cus_email: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cus_phone: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  usr_uuid: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cus_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  cus_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  timestamps: false,
+  tableName: 'cus_customers'
+});
+SequelizeCustomer.sync();
