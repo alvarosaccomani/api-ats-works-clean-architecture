92d9307ad83fb1cdb372d21df733fac8ac3d32c2
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-27 18:41:02 -0300

    i add customer.entity.ts into customer folder into folder domain to project

---
+++ src/domain/customer/customer.entity.ts
@@ -0,0 +1,10 @@
+export interface CustomerEntity {
+    cmp_uuid: string,
+    cus_uuid: string,
+    cus_fullname: string,
+    cus_email: string,
+    cus_phone: string,    
+    usr_uuid: string,
+    cus_createdat: Date,
+    cus_updatedat: Date
+}
