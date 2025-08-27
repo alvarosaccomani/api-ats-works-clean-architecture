47fd146ab07d7bc7a35410917b3d25e9abcd6ea4
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-27 18:41:58 -0300

    i add customer.value.ts into customer folder into folder domain to project

---
+++ src/domain/customer/customer.value.ts
@@ -0,0 +1,43 @@
+import { v4 as uuid } from "uuid";
+import moment from 'moment';
+import { CustomerEntity } from "./customer.entity";
+
+export class CustomerValue implements CustomerEntity {
+    cmp_uuid: string;
+    cus_uuid: string;
+    cus_fullname: string;
+    cus_email: string;
+    cus_phone: string;    
+    usr_uuid: string;
+    cus_createdat: Date;
+    cus_updatedat: Date;
+    
+    constructor({
+            cmp_uuid,
+            cus_uuid,
+            cus_fullname,
+            cus_email,
+            cus_phone,    
+            usr_uuid,
+            cus_createdat,
+            cus_updatedat
+        }:{ 
+            cmp_uuid: string,
+            cus_uuid: string,
+            cus_fullname: string,
+            cus_email: string,
+            cus_phone: string,    
+            usr_uuid: string,
+            cus_createdat: Date,
+            cus_updatedat: Date
+        }) {
+        this.cmp_uuid = cmp_uuid;
+        this.cus_uuid = uuid();
+        this.cus_fullname = cus_fullname;
+        this.cus_email = cus_email;
+        this.cus_phone = cus_phone;  
+        this.usr_uuid = usr_uuid;
+        this.cus_createdat = cus_createdat ?? moment().toDate();
+        this.cus_updatedat = cus_updatedat ?? moment().toDate();
+    }
+}
