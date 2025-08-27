2bb0d870341e96d5bcec9b2def6b44ab08265da2
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-26 22:04:08 -0300

    i add detail-model-item.value.ts into detail-model-item folder into folder domain to project

---
+++ src/domain/detail-model-item/detail-model-item.value.ts
@@ -0,0 +1,64 @@
+import { v4 as uuid } from "uuid";
+import moment from 'moment';
+import { DetailModelItemEntity } from "./detail-model-item.entity";
+
+export class DetailModelItemValue implements DetailModelItemEntity {
+    cmp_uuid: string;
+    itm_uuid: string;
+    cmpitm_uuid: string;
+    mitm_uuid: string;	
+	dmitm_uuid: string;	
+    dmitm_key: string;	
+    dmitm_name: string;
+    dmitm_description: string;
+    dtp_uuid: string;
+    dmitm_defaultvalue: string;
+    dmitm_active: boolean;
+	dmitm_createdat: Date;
+    dmitm_updatedat: Date;
+	
+    
+    constructor({
+            cmp_uuid,
+            itm_uuid,
+            cmpitm_uuid,
+            mitm_uuid,	
+            dmitm_uuid,
+            dmitm_key,
+            dmitm_name,
+            dmitm_description,
+            dtp_uuid,
+            dmitm_defaultvalue,
+            dmitm_active,
+            dmitm_createdat,
+            dmitm_updatedat
+        }:{ 
+            cmp_uuid: string,
+            itm_uuid: string,
+            cmpitm_uuid: string,
+            mitm_uuid: string,
+            dmitm_uuid: string,
+            dmitm_key: string,
+            dmitm_name: string,
+            dmitm_description: string,
+            dtp_uuid: string,
+            dmitm_defaultvalue: string,
+            dmitm_active: boolean,
+            dmitm_createdat: Date,
+            dmitm_updatedat: Date
+        }) {
+        this.cmp_uuid = cmp_uuid;
+        this.itm_uuid = itm_uuid;
+        this.cmpitm_uuid = cmpitm_uuid;
+        this.mitm_uuid = mitm_uuid;
+        this.dmitm_uuid = uuid();
+        this.dmitm_key = dmitm_key;
+        this.dmitm_name = dmitm_name;
+        this.dmitm_description = dmitm_description;
+        this.dtp_uuid = dtp_uuid;
+        this.dmitm_defaultvalue = dmitm_defaultvalue;
+        this.dmitm_active = dmitm_active;
+        this.dmitm_createdat = dmitm_createdat ?? moment().toDate();
+        this.dmitm_updatedat = dmitm_updatedat ?? moment().toDate();
+    }
+}
