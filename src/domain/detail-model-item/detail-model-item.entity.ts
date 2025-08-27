d0d7904dfba1053832615f8ad8ad931fcf1c6b1e
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-26 22:03:15 -0300

    i add detail-model-item.entity.ts into detail-model-item folder into folder domain to project

---
+++ src/domain/detail-model-item/detail-model-item.entity.ts
@@ -0,0 +1,15 @@
+export interface DetailModelItemEntity {
+    cmp_uuid: string,
+    itm_uuid: string,
+    cmpitm_uuid: string,
+    mitm_uuid: string,	
+	dmitm_uuid: string,	
+    dmitm_key: string,	
+    dmitm_name: string,
+    dmitm_description: string,
+    dtp_uuid: string,
+    dmitm_defaultvalue: string,
+    dmitm_active: boolean,
+	dmitm_createdat: Date,
+    dmitm_updatedat: Date
+}
