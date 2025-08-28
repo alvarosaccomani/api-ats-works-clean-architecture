e5ef6657a2e4322134733d6640e5f9a56d73ecae
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-28 00:32:40 -0300

    i add work-attachment.entity.ts into work-attachment folder into folder domain to project

---
+++ src/domain/work-attachment/work-attachment.entity.ts
@@ -0,0 +1,9 @@
+export interface WorkAttachmentEntity {
+    cmp_uuid: string,
+    wrk_uuid: string,
+    wrka_uuid: string,
+    wrka_attachmenttype: string,	
+	wrka_filepath: string,
+    wrka_createdat: Date,
+    wrka_updatedat: Date
+}
