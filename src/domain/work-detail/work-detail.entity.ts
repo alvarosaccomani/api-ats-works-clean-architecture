32bd98838f1d4d003f97e69e33e109472c38c96c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-27 23:56:55 -0300

    i add work-detail.entity.ts into work-detail folder into folder domain to project

---
+++ src/domain/work-detail/work-detail.entity.ts
@@ -0,0 +1,12 @@
+export interface WorkDetailEntity {
+    cmp_uuid: string,
+    wrk_uuid: string,
+    wrkd_uuid: string,
+    wrkd_key: string,	
+	wrkd_name: string,
+    wrkd_description: string,
+    dtp_uuid: string,
+	wrkd_value: string,
+	wrkd_createdat: Date,
+    wrkd_updatedat: Date
+}
