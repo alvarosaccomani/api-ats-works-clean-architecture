5ce88edcb11f95f1f22f821b1617f669ad11126c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-27 22:00:15 -0300

    i add work-state.entity.ts into work-state folder into folder domain to project

---
+++ src/domain/work-state/work-state.entity.ts
@@ -0,0 +1,10 @@
+export interface WorkStateEntity {
+    cmp_uuid: string,
+    wrks_uuid: string,
+    wrks_name: string,
+    wrks_description: string,
+    wrks_bkcolor: string,
+    wrks_frcolor: string,
+    wrks_createdat: Date,
+    wrks_updatedat: Date
+}
