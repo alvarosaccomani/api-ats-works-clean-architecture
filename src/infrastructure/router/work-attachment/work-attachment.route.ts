b953d01de29b4673ea28f8253c2cf0006fb5217d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-28 00:35:27 -0300

    i add work-attachment.route.ts into work-attachment folder into folder router into folder infrastructure to project

---
+++ src/infrastructure/router/work-attachment/work-attachment.route.ts
@@ -0,0 +1,33 @@
+import { Express } from "express";
+import { SequelizeRepository } from "../../repository/work-attachment/sequelize-work-attachment.repository";
+import { WorkAttachmentUseCase } from "../../../application/work-attachment/work-attachment-use-case";
+import { WorkAttachmentController } from "../../controller/work-attachment/work-attachment.controller";
+import SocketAdapter from "../../services/socketAdapter";
+
+function configureWorkAttachmentRoutes(app: Express, socketAdapter: SocketAdapter) {
+    /*
+    *   Iniciar repository
+    */
+    
+    const sequelizeWorkAttachmentRepository = new SequelizeRepository();
+    
+    /*
+    *   Iniciar casos de uso
+    */
+    
+    const workAttachmentUseCase = new WorkAttachmentUseCase(sequelizeWorkAttachmentRepository);
+    
+    /*
+    *   Iniciar controller
+    */
+    
+    const workAttachmentCtrl = new WorkAttachmentController(workAttachmentUseCase, socketAdapter);
+    
+    app.get(`/${process.env.BASE_URL_API}/work-attachments/:filter?/:page?/:perPage?`, workAttachmentCtrl.getAllCtrl);
+    app.get(`/${process.env.BASE_URL_API}/work-attachment/:cmp_uuid/:wrk_uuid/:wrka_uuid`, workAttachmentCtrl.getCtrl);
+    app.post(`/${process.env.BASE_URL_API}/work-attachment`, workAttachmentCtrl.insertCtrl);
+    app.put(`/${process.env.BASE_URL_API}/work-attachment/:cmp_uuid/:wrk_uuid/:wrka_uuid`, workAttachmentCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/work-attachment/:cmp_uuid/:wrk_uuid/:wrka_uuid`, workAttachmentCtrl.deleteCtrl);
+}
+
+export default configureWorkAttachmentRoutes;
