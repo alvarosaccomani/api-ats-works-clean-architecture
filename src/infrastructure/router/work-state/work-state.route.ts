d2fe3b021a323d29ec68438da2214cbf83d72cac
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-27 22:04:03 -0300

    i add work-state.route.ts into work-state folder into folder router into folder infrastructure to project

---
+++ src/infrastructure/router/work-state/work-state.route.ts
@@ -0,0 +1,33 @@
+import { Express } from "express";
+import { SequelizeRepository } from "../../repository/work-state/sequelize-work-state.repository";
+import { WorkStateUseCase } from "../../../application/work-state/work-state-use-case";
+import { WorkStateController } from "../../controller/work-state/work-state.controller";
+import SocketAdapter from "../../services/socketAdapter";
+
+function configureWorkStateRoutes(app: Express, socketAdapter: SocketAdapter) {
+    /*
+    *   Iniciar repository
+    */
+    
+    const sequelizeWorkStateRepository = new SequelizeRepository();
+    
+    /*
+    *   Iniciar casos de uso
+    */
+    
+    const workStateUseCase = new WorkStateUseCase(sequelizeWorkStateRepository);
+    
+    /*
+    *   Iniciar controller
+    */
+    
+    const workStateCtrl = new WorkStateController(workStateUseCase, socketAdapter);
+    
+    app.get(`/${process.env.BASE_URL_API}/work-states/:filter?/:page?/:perPage?`, workStateCtrl.getAllCtrl);
+    app.get(`/${process.env.BASE_URL_API}/work-state/:cmp_uuid/:wrks_uuid`, workStateCtrl.getCtrl);
+    app.post(`/${process.env.BASE_URL_API}/work-state`, workStateCtrl.insertCtrl);
+    app.put(`/${process.env.BASE_URL_API}/work-state/:cmp_uuid/:wrks_uuid`, workStateCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/work-state/:cmp_uuid/:wrks_uuid`, workStateCtrl.deleteCtrl);
+}
+
+export default configureWorkStateRoutes;
