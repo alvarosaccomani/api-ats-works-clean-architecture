c013073332a75702968ea6733c36455e60494612
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-27 22:02:04 -0300

    i add work-state.model.ts into work-state folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/work-state/work-state.model.ts
@@ -0,0 +1,56 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { WorkStateEntity } from "../../../domain/work-state/work-state.entity";
+
+export class SequelizeWorkState extends Model<WorkStateEntity, Omit<WorkStateEntity, 'id'>> {
+  declare cmp_uuid: string;
+  declare wrks_uuid: string;
+  declare wrks_name: string;
+  declare wrks_description: string;
+  declare wrks_bkcolor: string;
+  declare wrks_frcolor: string;
+  declare wrks_createdat: Date;
+  declare wrks_updatedat: Date;
+}
+
+SequelizeWorkState.init({
+  cmp_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true/*,
+    autoIncrement: true*/
+  },
+  wrks_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true/*,
+    autoIncrement: true*/
+  },
+  wrks_name: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  wrks_description: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  wrks_bkcolor: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  wrks_frcolor: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  wrks_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  wrks_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  timestamps: false,
+  tableName: 'wrks_workstates'
+});
+SequelizeWorkState.sync();
