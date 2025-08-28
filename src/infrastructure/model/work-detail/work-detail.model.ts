b0e981f966b0d588d8fd658e2105bd209ccba7be
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-08-27 23:58:12 -0300

    i add work-detail.model.ts into work-detail folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/work-detail/work-detail.model.ts
@@ -0,0 +1,64 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { WorkDetailEntity } from "../../../domain/work-detail/work-detail.entity";
+
+export class SequelizeWorkDetail extends Model<WorkDetailEntity, Omit<WorkDetailEntity, 'id'>> {
+  declare cmp_uuid: string;
+  declare wrk_uuid: string;
+  declare wrkd_uuid: string;
+  declare wrkd_key: string;
+	declare wrkd_name: string;
+  declare wrkd_description: string;
+  declare dtp_uuid: string;
+	declare wrkd_value: string;
+	declare wrkd_createdat: Date;
+  declare wrkd_updatedat: Date;
+}
+
+SequelizeWorkDetail.init({
+  cmp_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true
+  },
+  wrk_uuid: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+  wrkd_uuid: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  wrkd_key: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  wrkd_name: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+	wrkd_description: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  dtp_uuid: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+	wrkd_value: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  wrkd_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  wrkd_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  timestamps: false,
+  tableName: 'wrkd_workdetails'
+});
+SequelizeWorkDetail.sync();
