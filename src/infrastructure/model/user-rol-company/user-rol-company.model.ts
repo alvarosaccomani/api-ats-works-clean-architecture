f2e5d22d2382a6070c7c54c6ec6ad1f505588b52
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-17 22:29:24 -0300

    i add the relationship to usr table in the user-rol-companyl model

--- src/infrastructure/model/user-rol-company/user-rol-company.model.ts
+++ src/infrastructure/model/user-rol-company/user-rol-company.model.ts
@@ -2,6 +2,7 @@ import { DataTypes, Model } from 'sequelize';
 import { sequelize } from '../../db/sequelize';
 import { UserRolCompanyEntity } from "../../../domain/user-rol-company/user-rol-company.entity";
 import { SequelizeCompany } from '../company/company.model';
+import { SequelizeUser } from '../user/user.model';
 import { SequelizeRol } from '../rol/rol.model';
 
 export class SequelizeUserRolCompany extends Model<UserRolCompanyEntity, Omit<UserRolCompanyEntity, 'id'>> {
@@ -46,6 +47,10 @@ SequelizeUserRolCompany.init({
 SequelizeUserRolCompany.belongsTo(SequelizeCompany, {
   as: 'cmp',
   foreignKey: 'cmp_uuid'
+});
+SequelizeUserRolCompany.belongsTo(SequelizeUser, {
+  as: 'usr',
+  foreignKey: 'usr_uuid'
 });
 SequelizeUserRolCompany.belongsTo(SequelizeRol, {
   as: 'rol',
