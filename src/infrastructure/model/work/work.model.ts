83f1e713e65dfe8c2786d52173caa1ec240cdf43
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-25 19:18:14 -0300

    i add the relationships adr, wrks, wrk_user, wrk_operator, mitm and workDetails tables in the work model

--- src/infrastructure/model/work/work.model.ts
+++ src/infrastructure/model/work/work.model.ts
@@ -1,6 +1,10 @@
 import { DataTypes, Model } from 'sequelize';
 import { sequelize } from '../../db/sequelize';
 import { WorkEntity } from "../../../domain/work/work.entity";
+import { SequelizeAddress } from '../address/address.model';
+import { SequelizeWorkState } from '../work-state/work-state.model';
+import { SequelizeUser } from '../user/user.model';
+import { SequelizeModelItem } from '../model-item/model-item.model';
 import { SequelizeWorkDetail } from "../work-detail/work-detail.model";
 
 export class SequelizeWork extends Model<WorkEntity, Omit<WorkEntity, 'id'>> {
@@ -87,9 +91,70 @@ SequelizeWork.init({
   timestamps: false,
   tableName: 'wrk_works'
 });
+
+//Sequelize Address Foreign Key
+SequelizeWork.belongsTo(SequelizeAddress, {
+    foreignKey: "cmp_uuid",
+    targetKey: "cmp_uuid"
+});
+SequelizeWork.belongsTo(SequelizeAddress, {
+    foreignKey: 'adr_uuid',
+    targetKey: "adr_uuid",
+    as: 'adr'
+});
+
+//Sequelize Work State Foreign Key
+SequelizeWork.belongsTo(SequelizeWorkState, {
+    foreignKey: "cmp_uuid",
+    targetKey: "cmp_uuid"
+});
+SequelizeWork.belongsTo(SequelizeWorkState, {
+    foreignKey: 'wrks_uuid',
+    targetKey: "wrks_uuid",
+    as: 'wrks'
+});
+
+//Sequelize User Work Foreign Key
+SequelizeWork.belongsTo(SequelizeUser, {
+    foreignKey: 'wrk_user_uuid',
+    targetKey: "usr_uuid",
+    as: 'wrk_user'
+});
+
+//Sequelize Operator Work Foreign Key
+SequelizeWork.belongsTo(SequelizeUser, {
+    foreignKey: 'wrk_operator_uuid',
+    targetKey: "usr_uuid",
+    as: 'wrk_operator'
+});
+
+//Sequelize Model Item Foreign Key
+SequelizeWork.belongsTo(SequelizeModelItem, {
+    foreignKey: "cmp_uuid",
+    targetKey: "cmp_uuid"
+});
+SequelizeWork.belongsTo(SequelizeModelItem, {
+    foreignKey: "itm_uuid",
+    targetKey: "itm_uuid"
+});
+SequelizeWork.belongsTo(SequelizeModelItem, {
+    foreignKey: "cmpitm_uuid",
+    targetKey: "cmpitm_uuid"
+});
+SequelizeWork.belongsTo(SequelizeModelItem, {
+    foreignKey: "mitm_uuid",
+    targetKey: "mitm_uuid",
+    as: 'mitm'
+});
+
+//Sequelize Work Detail Foreign Key
+SequelizeWork.hasMany(SequelizeWorkDetail, {
+    foreignKey: 'cmp_uuid',
+    sourceKey: 'cmp_uuid'
+});
 SequelizeWork.hasMany(SequelizeWorkDetail, {
     foreignKey: 'wrk_uuid',
     sourceKey: 'wrk_uuid',
-    as: 'workDetail'
+    as: 'workDetails'
 });
 SequelizeWork.sync();
