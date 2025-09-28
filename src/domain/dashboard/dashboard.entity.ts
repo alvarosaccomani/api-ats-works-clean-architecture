15fbb48fa1b10ba1274733306c4eb69451396c0f
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-28 18:03:14 -0300

    i add dashboard.entity.ts into dashboard folder into folder domain to project

---
+++ src/domain/dashboard/dashboard.entity.ts
@@ -0,0 +1,9 @@
+export interface DashboardEntity {
+    cmp_uuid: string,
+    companiesCount: string,
+    usersCount: string,    
+    itemsCount: string,    
+    modelsItemsCount: string,
+    worksCount: string,
+    customersCount: string
+}
