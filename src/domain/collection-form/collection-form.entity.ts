69224f8c3f8d74c0c4cdad4f6916dafe98111eaf
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-30 10:40:58 -0300

    i add collection-form.entity.ts into collection-form folder into folder domain to project

---
+++ src/domain/collection-form/collection-form.entity.ts
@@ -0,0 +1,11 @@
+export interface CollectionFormEntity {
+    cmp_uuid: string,
+    cfrm_uuid: string,
+    cfrm_name: string,
+    cfrm_order: number,
+    cfrm_bkcolor: string,    
+    cfrm_frcolor: string,
+    cfrm_active: boolean,
+    cfrm_createdat: Date,
+    cfrm_updatedat: Date
+}
