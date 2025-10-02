32d01944c5ee656c4e8c223c44d4ee2182a0da95
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-02 09:41:38 -0300

    i add pagination to work-detail.controller.ts

--- src/infrastructure/controller/work-detail/work-detail.controller.ts
+++ src/infrastructure/controller/work-detail/work-detail.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { WorkDetailUseCase } from "../../../application/work-detail/work-detail-use-case";
 import SocketAdapter from "../../services/socketAdapter";
+import { paginator } from "../../services/paginator.service";
 
 export class WorkDetailController {
     constructor(private workDetailUseCase: WorkDetailUseCase, private socketAdapter: SocketAdapter) {
@@ -14,6 +15,8 @@ export class WorkDetailController {
     public async getAllCtrl(req: Request, res: Response) {
         try {
             const cmp_uuid = req.params.cmp_uuid;
+            const page = (req.params.page ? parseInt(req.params.page) : null);
+            const perPage = (req.params.perPage ? parseInt(req.params.perPage) : null);
             if(!cmp_uuid || cmp_uuid.toLowerCase() === 'null' || cmp_uuid.toLowerCase() === 'undefined') {
                 return res.status(400).json({
                     success: false,
@@ -21,12 +24,21 @@ export class WorkDetailController {
                     error: 'Debe proporcionar un Id de company.'
                 });
             }
-            const workDetails = await this.workDetailUseCase.getWorkDetails(cmp_uuid)
-            return res.status(200).send({
-                success: true,
-                message: 'Work details retornados.',
-                data: workDetails
-            });
+            if (page && perPage) {
+                const workDetails = await this.workDetailUseCase.getWorkDetails(cmp_uuid)
+                return res.status(200).send({
+                    success: true,
+                    message: 'Work details retornados.',
+                    ...paginator(workDetails, page, perPage)
+                });
+            } else {
+                const workDetails = await this.workDetailUseCase.getWorkDetails(cmp_uuid)
+                return res.status(200).send({
+                    success: true,
+                    message: 'Work details retornados.',
+                    data: workDetails
+                });
+            }
         } catch (error: any) {
             console.error('Error en getAllCtrl (controller):', error.message);
             return res.status(400).json({
