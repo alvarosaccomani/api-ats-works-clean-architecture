be1a3b23db5261e817aad365d5f4f833e929a83e
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-06 18:29:24 -0300

    i replace rol parameter at updateRol in rol.repository.ts

--- src/domain/rol/rol.repository.ts
+++ src/domain/rol/rol.repository.ts
@@ -1,10 +1,10 @@
-import { RolEntity } from "./rol.entity";
+import { RolEntity, RolUpdateData } from "./rol.entity";
 
 export interface RolRepository {
     getRoles(): Promise<RolEntity[] | null>;
     findRolById(rol_uuid: string): Promise<RolEntity | null>;
     createRol(rol: RolEntity): Promise<RolEntity | null>;
-    updateRol(rol_uuid: string, rol: RolEntity): Promise<RolEntity | null>;
+    updateRol(rol_uuid: string, rol: RolUpdateData): Promise<RolEntity | null>;
     deleteRol(rol_uuid: string): Promise<RolEntity | null>;
     findRolByName(rol_name: string, excludeUuid?: string | null): Promise<RolEntity | null>;
 }
