eee455a2caacc0bee730a774d799367908868ff6
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-06 21:55:45 -0300

    i replace address parameter at updateAddress in address.repository.ts

--- src/domain/address/address.repository.ts
+++ src/domain/address/address.repository.ts
@@ -1,10 +1,10 @@
-import { AddressEntity } from "./address.entity";
+import { AddressEntity, AddressUpdateData } from "./address.entity";
 
 export interface AddressRepository {
     getAddresses(cmp_uuid: string, cus_uuid: string): Promise<AddressEntity[] | null>;
     findAddressById(cmp_uuid: string, cus_uuid: string, adr_uuid: string): Promise<AddressEntity | null>;
     createAddress(address: AddressEntity): Promise<AddressEntity | null>;
-    updateAddress(cmp_uuid: string, cus_uuid: string, adr_uuid: string, address: AddressEntity): Promise<AddressEntity | null>;
+    updateAddress(cmp_uuid: string, cus_uuid: string, adr_uuid: string, address: AddressUpdateData): Promise<AddressEntity | null>;
     deleteAddress(cmp_uuid: string, cus_uuid: string, adr_uuid: string): Promise<AddressEntity | null>;
     findAddressByName(cmp_uuid: string, cus_uuid: string, adr_address: string, excludeUuid?: string | null): Promise<AddressEntity | null>;
 }
