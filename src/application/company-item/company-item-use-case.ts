7de9998c9cd121c6590a09e4764923c51ee2663f
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-08 10:20:58 -0300

    i add util TimezoneConverter to company-item-use-case.ts

--- src/application/company-item/company-item-use-case.ts
+++ src/application/company-item/company-item-use-case.ts
@@ -1,5 +1,6 @@
 import { CompanyItemRepository } from "../../domain/company-item/company-item.repository";
 import { CompanyItemValue } from "../../domain/company-item/company-item.value";
+import { TimezoneConverter } from "../../infrastructure/utils/TimezoneConverter";
 
 export class CompanyItemUseCase {
     constructor(
@@ -14,11 +15,18 @@ export class CompanyItemUseCase {
 
     public async getCompanyItems(cmp_uuid: string) {
         try {
-            const companyItems = this.companyItemRepository.getCompanyItems(cmp_uuid);
+            const companyItems = await this.companyItemRepository.getCompanyItems(cmp_uuid);
             if(!companyItems) {
                 throw new Error('No hay companies.');
             }
-            return companyItems;
+            return companyItems.map((companyItem) => ({
+                cmp_uuid: companyItem.cmp_uuid,
+                itm_uuid: companyItem.itm_uuid,
+                itm: companyItem.itm,
+                cmpitm_uuid: companyItem.cmpitm_uuid,
+                cmpitm_createdat: TimezoneConverter.toIsoStringInTimezone(companyItem.cmpitm_createdat, 'America/Buenos_Aires'),
+                cmpitm_updatedat: TimezoneConverter.toIsoStringInTimezone(companyItem.cmpitm_updatedat, 'America/Buenos_Aires')
+            }));
         } catch (error: any) {
             console.error('Error en getCompanyItems (use case):', error.message);
             throw error; // Propagar el error hacia el controlador
@@ -27,11 +35,18 @@ export class CompanyItemUseCase {
 
     public async getDetailCompanyItem(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string) {
         try {
-            const companyItems = this.companyItemRepository.findCompanyItemById(cmp_uuid, itm_uuid, cmpitm_uuid);
-            if(!companyItems) {
+            const companyItem = await this.companyItemRepository.findCompanyItemById(cmp_uuid, itm_uuid, cmpitm_uuid);
+            if(!companyItem) {
                 throw new Error(`No hay company items con el Id: ${cmp_uuid}, ${itm_uuid}, ${cmpitm_uuid}`);
             }
-            return companyItems;
+            return {
+                cmp_uuid: companyItem.cmp_uuid,
+                itm_uuid: companyItem.itm_uuid,
+                itm: companyItem.itm,
+                cmpitm_uuid: companyItem.cmpitm_uuid,
+                cmpitm_createdat: TimezoneConverter.toIsoStringInTimezone(companyItem.cmpitm_createdat, 'America/Buenos_Aires'),
+                cmpitm_updatedat: TimezoneConverter.toIsoStringInTimezone(companyItem.cmpitm_updatedat, 'America/Buenos_Aires')
+            };
         } catch (error: any) {
             console.error('Error en getDetailCompanyItems (use case):', error.message);
             throw error; // Propagar el error hacia el controlador
@@ -45,7 +60,13 @@ export class CompanyItemUseCase {
             if(!companyItemsCreated) {
                 throw new Error(`No se pudo insertar el company item.`);
             }
-            return companyItemsCreated;
+            return {
+                cmp_uuid: companyItemsCreated.cmp_uuid,
+                itm_uuid: companyItemsCreated.itm_uuid,
+                cmpitm_uuid: companyItemsCreated.cmpitm_uuid,
+                cmpitm_createdat: TimezoneConverter.toIsoStringInTimezone(companyItemsCreated.cmpitm_createdat, 'America/Buenos_Aires'),
+                cmpitm_updatedat: TimezoneConverter.toIsoStringInTimezone(companyItemsCreated.cmpitm_updatedat, 'America/Buenos_Aires')
+            };
         } catch (error: any) {
             console.error('Error en createCompanyItems (use case):', error.message);
             throw error; // Propagar el error hacia el controlador
@@ -58,7 +79,13 @@ export class CompanyItemUseCase {
             if(!companyItemsUpdated) {
                 throw new Error(`No se pudo actualizar el company item.`);
             }
-            return companyItemsUpdated;
+            return {
+                cmp_uuid: companyItemsUpdated.cmp_uuid,
+                itm_uuid: companyItemsUpdated.itm_uuid,
+                cmpitm_uuid: companyItemsUpdated.cmpitm_uuid,
+                cmpitm_createdat: TimezoneConverter.toIsoStringInTimezone(companyItemsUpdated.cmpitm_createdat, 'America/Buenos_Aires'),
+                cmpitm_updatedat: TimezoneConverter.toIsoStringInTimezone(companyItemsUpdated.cmpitm_updatedat, 'America/Buenos_Aires')
+            };
         } catch (error: any) {
             console.error('Error en updateCompanyItems (use case):', error.message);
             throw error; // Propagar el error hacia el controlador
@@ -71,7 +98,13 @@ export class CompanyItemUseCase {
             if(!companyItemsDeleted) {
                 throw new Error(`No se pudo eliminar el company item.`);
             }
-            return companyItemsDeleted;
+            return {
+                cmp_uuid: companyItemsDeleted.cmp_uuid,
+                itm_uuid: companyItemsDeleted.itm_uuid,
+                cmpitm_uuid: companyItemsDeleted.cmpitm_uuid,
+                cmpitm_createdat: TimezoneConverter.toIsoStringInTimezone(companyItemsDeleted.cmpitm_createdat, 'America/Buenos_Aires'),
+                cmpitm_updatedat: TimezoneConverter.toIsoStringInTimezone(companyItemsDeleted.cmpitm_updatedat, 'America/Buenos_Aires')
+            };
         } catch (error: any) {
             console.error('Error en deleteCompanyItems (use case):', error.message);
             throw error; // Propagar el error hacia el controlador
