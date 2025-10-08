060aa108df991f842401eff9614314536194de08
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-08 14:34:29 -0300

    i replace customer parameter at updateCustomer in customer.repository.ts

--- src/domain/customer/customer.repository.ts
+++ src/domain/customer/customer.repository.ts
@@ -1,10 +1,10 @@
-import { CustomerEntity } from "./customer.entity";
+import { CustomerEntity, CustomerUpdateData } from "./customer.entity";
 
 export interface CustomerRepository {
     getCustomers(cmp_uuid: string): Promise<CustomerEntity[] | null>;
     findCustomerById(cmp_uuid: string, cus_uuid: string): Promise<CustomerEntity | null>;
     createCustomer(customer: CustomerEntity): Promise<CustomerEntity | null>;
-    updateCustomer(cmp_uuid: string, cus_uuid: string, customer: CustomerEntity): Promise<CustomerEntity | null>;
+    updateCustomer(cmp_uuid: string, cus_uuid: string, customer: CustomerUpdateData): Promise<CustomerEntity | null>;
     deleteCustomer(cmp_uuid: string, cus_uuid: string): Promise<CustomerEntity | null>;
     findCustomerByName(cmp_uuid: string, cus_fullname: string, excludeUuid?: string | null): Promise<CustomerEntity | null>;
 }
