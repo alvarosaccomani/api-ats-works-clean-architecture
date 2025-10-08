e3437a8f2ecc7b22f96273139b4a1fcdc2278f75
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-08 09:52:09 -0300

    i add itm property to company-item.entity.ts

--- src/domain/company-item/company-item.entity.ts
+++ src/domain/company-item/company-item.entity.ts
@@ -1,6 +1,9 @@
+import { ItemEntity } from "../item/item.entity";
+
 export interface CompanyItemEntity {
     cmp_uuid: string,
     itm_uuid: string,
+    itm?: ItemEntity,
     cmpitm_uuid: string,
     cmpitm_createdat: Date,
     cmpitm_updatedat: Date
