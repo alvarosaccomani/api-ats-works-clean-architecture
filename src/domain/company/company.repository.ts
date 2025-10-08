c098c62873a8fd75f8ba949ee5dab28034e7f56a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-08 09:24:37 -0300

    i replace company parameter at updateCompany in company.repository.ts

--- src/domain/company/company.repository.ts
+++ src/domain/company/company.repository.ts
@@ -1,10 +1,10 @@
-import { CompanyEntity } from "./company.entity";
+import { CompanyEntity, CompanyUpdateData } from "./company.entity";
 
 export interface CompanyRepository {
     getCompanies(): Promise<CompanyEntity[] | null>;
     findCompanyById(cmp_uuid: string): Promise<CompanyEntity | null>;
     createCompany(company: CompanyEntity): Promise<CompanyEntity | null>;
-    updateCompany(cmp_uuid: string, company: CompanyEntity): Promise<CompanyEntity | null>;
+    updateCompany(cmp_uuid: string, company: CompanyUpdateData): Promise<CompanyEntity | null>;
     deleteCompany(cmp_uuid: string): Promise<CompanyEntity | null>;
     findCompanyByName(cmp_name: string, excludeUuid?: string | null): Promise<CompanyEntity | null>;
 }
