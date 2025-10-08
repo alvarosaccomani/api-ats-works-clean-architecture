aec450fcb7ffd96a75003e8080956322b0b7a382
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-08 08:44:00 -0300

    i replace collectionForm parameter at updateCollectionForm in collection-form.repository.ts

--- src/domain/collection-form/collection-form.repository.ts
+++ src/domain/collection-form/collection-form.repository.ts
@@ -1,10 +1,10 @@
-import { CollectionFormEntity } from "./collection-form.entity";
+import { CollectionFormEntity, CollectionFormUpdateData } from "./collection-form.entity";
 
 export interface CollectionFormRepository {
     getCollectionForms(cmp_uuid: string): Promise<CollectionFormEntity[] | null>;
     findCollectionFormById(cmp_uuid: string, cfrm_uuid: string): Promise<CollectionFormEntity | null>;
     createCollectionForm(collectionForm: CollectionFormEntity): Promise<CollectionFormEntity | null>;
-    updateCollectionForm(cmp_uuid: string, cfrm_uuid: string, collectionForm: CollectionFormEntity): Promise<CollectionFormEntity | null>;
+    updateCollectionForm(cmp_uuid: string, cfrm_uuid: string, collectionForm: CollectionFormUpdateData): Promise<CollectionFormEntity | null>;
     deleteCollectionForm(cmp_uuid: string, cfrm_uuid: string): Promise<CollectionFormEntity | null>;
     findCollectionFormByName(cmp_uuid: string, cus_fullname: string, excludeUuid?: string | null): Promise<CollectionFormEntity | null>;
 }
