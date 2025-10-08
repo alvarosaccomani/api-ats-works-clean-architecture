bf12f7000cef5a9c77cdc30913509651552a8cb5
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-08 17:42:09 -0300

    i replace item parameter at updateItem in item.repository.ts

--- src/domain/item/item.repository.ts
+++ src/domain/item/item.repository.ts
@@ -1,10 +1,10 @@
-import { ItemEntity } from "./item.entity";
+import { ItemEntity, ItemUpdateData } from "./item.entity";
 
 export interface ItemRepository {
     getItems(): Promise<ItemEntity[] | null>;
     findItemById(itm_uuid: string): Promise<ItemEntity | null>;
     createItem(item: ItemEntity): Promise<ItemEntity | null>;
-    updateItem(itm_uuid: string, item: ItemEntity): Promise<ItemEntity | null>;
+    updateItem(itm_uuid: string, item: ItemUpdateData): Promise<ItemEntity | null>;
     deleteItem(itm_uuid: string): Promise<ItemEntity | null>;
     findItemByName(itm_name: string, excludeUuid?: string | null): Promise<ItemEntity | null>;
 }
