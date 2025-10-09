87ae3724ce83457c999ee559e098378db5f7f813
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-08 23:51:34 -0300

    i replace modelItem parameter at updateModelItem in model-item.repository.ts

--- src/domain/model-item/model-item.repository.ts
+++ src/domain/model-item/model-item.repository.ts
@@ -1,10 +1,10 @@
-import { ModelItemEntity } from "./model-item.entity";
+import { ModelItemEntity, ModelItemUpdateData } from "./model-item.entity";
 
 export interface ModelItemRepository {
     getModelItems(cmp_uuid: string): Promise<ModelItemEntity[] | null>;
     findModelItemById(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string): Promise<ModelItemEntity | null>;
     createModelItem(modelItem: ModelItemEntity): Promise<ModelItemEntity | null>;
-    updateModelItem(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string, modelItem: ModelItemEntity): Promise<ModelItemEntity | null>;
+    updateModelItem(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string, modelItem: ModelItemUpdateData): Promise<ModelItemEntity | null>;
     deleteModelItem(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string): Promise<ModelItemEntity | null>;
     existModelItem(cmp_uuid: string, itm_uuid: string, cmpitm_uuid: string, mitm_uuid: string): Promise<ModelItemEntity | null>;
 }
