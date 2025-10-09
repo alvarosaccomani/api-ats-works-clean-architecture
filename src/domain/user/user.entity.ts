6eb4495044b7ca7f8c68ef32765b77d77a69ad87
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-09 09:07:20 -0300

    i add UserUpdateData to user.entity.ts

--- src/domain/user/user.entity.ts
+++ src/domain/user/user.entity.ts
@@ -17,3 +17,6 @@ export interface UserEntity {
     usr_createdat: Date,
     usr_updatedat: Date,
 }
+
+//Update
+export type UserUpdateData = Pick<UserEntity, 'usr_name' | 'usr_surname' | 'usr_password' | 'usr_image' | 'usr_email' | 'usr_nick' | 'usr_bio' | 'usr_registered' | 'usr_socket' | 'usr_online' | 'usr_confirmed' | 'usr_confirmationtoken' | 'usr_resetpasswordtoken' | 'usr_resetpasswordexpires'>
