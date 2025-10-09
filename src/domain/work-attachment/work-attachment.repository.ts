721898c42b98574e00c30d50761f3abab5c753f1
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-09 18:50:16 -0300

    i replace workattachment parameter at updateWorkAttachment in work-attachment.repository.ts

--- src/domain/work-attachment/work-attachment.repository.ts
+++ src/domain/work-attachment/work-attachment.repository.ts
@@ -1,10 +1,10 @@
-import { WorkAttachmentEntity } from "./work-attachment.entity";
+import { WorkAttachmentEntity, WorkAttachmentUpdateData } from "./work-attachment.entity";
 
 export interface WorkAttachmentRepository {
     getWorkAttachments(cmp_uuid: string): Promise<WorkAttachmentEntity[] | null>;
     findWorkAttachmentById(cmp_uuid: string, wrk_uuid: string, wrka_uuid: string): Promise<WorkAttachmentEntity | null>;
     createWorkAttachment(workattachment: WorkAttachmentEntity): Promise<WorkAttachmentEntity | null>;
-    updateWorkAttachment(cmp_uuid: string, wrk_uuid: string, wrka_uuid: string, workattachment: WorkAttachmentEntity): Promise<WorkAttachmentEntity | null>;
+    updateWorkAttachment(cmp_uuid: string, wrk_uuid: string, wrka_uuid: string, workattachment: WorkAttachmentUpdateData): Promise<WorkAttachmentEntity | null>;
     deleteWorkAttachment(cmp_uuid: string, wrk_uuid: string, wrka_uuid: string): Promise<WorkAttachmentEntity | null>;
     existWorkAttachment(cmp_uuid: string, wrk_uuid: string, wrka_uuid: string): Promise<WorkAttachmentEntity | null>;
 }
