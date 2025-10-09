734aaae980c270bc4f432ae0f86103e2a904863b
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-09 09:09:18 -0300

    i replace user parameter at updateUser in user.repository.ts

--- src/domain/user/user.repository.ts
+++ src/domain/user/user.repository.ts
@@ -1,11 +1,11 @@
-import { UserEntity } from "./user.entity";
+import { UserEntity, UserUpdateData } from "./user.entity";
 
 export interface UserRepository {
     getUsers(): Promise<UserEntity[] | null>;
     findUserById(usr_uuid: string): Promise<UserEntity | null>;
     findUserByEmail(usr_email: string): Promise<UserEntity | null>;
     registerUser(user: UserEntity): Promise<UserEntity | null>;
-    updateUser(usr_uuid: string, user: UserEntity): Promise<UserEntity | null>;
+    updateUser(usr_uuid: string, user: UserUpdateData): Promise<UserEntity | null>;
     deleteUser(usr_uuid: string): Promise<UserEntity | null>;
     userExist(usr_nick: string, usr_email: string): Promise<UserEntity | null>;
     loginUser(usr_nick: string, usr_password: string, gettoken: boolean): Promise<UserEntity | String | null>;
