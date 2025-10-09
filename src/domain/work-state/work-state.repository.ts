dabb2556f58aa6cc859c45009ac732f532194f0f
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-09 19:53:28 -0300

    i replace workState parameter at updateWorkState in work-state.repository.ts

--- src/domain/work-state/work-state.repository.ts
+++ src/domain/work-state/work-state.repository.ts
@@ -1,10 +1,10 @@
-import { WorkStateEntity } from "./work-state.entity";
+import { WorkStateEntity, WorkStateUpdateData } from "./work-state.entity";
 
 export interface WorkStateRepository {
     getWorkStates(cmp_uuid: string): Promise<WorkStateEntity[] | null>;
     findWorkStateById(cmp_uuid: string, wrks_uuid: string): Promise<WorkStateEntity | null>;
     createWorkState(workState: WorkStateEntity): Promise<WorkStateEntity | null>;
-    updateWorkState(cmp_uuid: string, wrks_uuid: string, workState: WorkStateEntity): Promise<WorkStateEntity | null>;
+    updateWorkState(cmp_uuid: string, wrks_uuid: string, workState: WorkStateUpdateData): Promise<WorkStateEntity | null>;
     deleteWorkState(cmp_uuid: string, wrks_uuid: string): Promise<WorkStateEntity | null>;
     findWorkStateByName(cmp_uuid: string, wrks_name: string, excludeUuid?: string | null): Promise<WorkStateEntity | null>;
 }
