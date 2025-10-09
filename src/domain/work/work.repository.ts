08f7cfa7c88c0e90b7fc746ad1a004c71420c334
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-09 18:16:21 -0300

    i replace work parameter at updateWork in work.repository.ts

--- src/domain/work/work.repository.ts
+++ src/domain/work/work.repository.ts
@@ -1,10 +1,10 @@
-import { WorkEntity } from "./work.entity";
+import { WorkEntity, WorkUpdateData } from "./work.entity";
 
 export interface WorkRepository {
     getWorks(cmp_uuid: string): Promise<WorkEntity[] | null>;
     findWorkById(cmp_uuid: string, wrk_uuid: string): Promise<WorkEntity | null>;
     createWork(work: WorkEntity): Promise<WorkEntity | null>;
-    updateWork(cmp_uuid: string, wrk_uuid: string, work: WorkEntity): Promise<WorkEntity | null>;
+    updateWork(cmp_uuid: string, wrk_uuid: string, work: WorkUpdateData): Promise<WorkEntity | null>;
     deleteWork(cmp_uuid: string, wrk_uuid: string): Promise<WorkEntity | null>;
     existWork(cmp_uuid: string, wrk_uuid: string): Promise<WorkEntity | null>;
 }
