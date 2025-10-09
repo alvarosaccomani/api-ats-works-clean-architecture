f4603e0d58c357c7792b835026a1b3e3f0de1f2c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-09 10:05:20 -0300

    i add adr, wrks,  wrk_user, wrk_operator,  mitm, workDetails and workAttachments properties to work.entity.ts

--- src/domain/work/work.entity.ts
+++ src/domain/work/work.entity.ts
@@ -1,17 +1,31 @@
+import { AddressEntity } from "../address/address.entity";
+import { ModelItemEntity } from "../model-item/model-item.entity";
+import { UserEntity } from "../user/user.entity";
+import { WorkAttachmentEntity } from "../work-attachment/work-attachment.entity";
+import { WorkDetailEntity } from "../work-detail/work-detail.entity";
+import { WorkStateEntity } from "../work-state/work-state.entity";
+
 export interface WorkEntity {
     cmp_uuid: string,
     wrk_uuid: string,
     adr_uuid: string,
+    adr?: AddressEntity,
     wrk_description: string,	
 	wrk_workdate: Date,
     wrk_workdateinit: Date,
     wrk_workdatefinish: Date,
 	wrks_uuid: string,
+    wrks?: WorkStateEntity,
     wrk_user_uuid: string,
+    wrk_user?: UserEntity,
     wrk_operator_uuid: string,
+    wrk_operator?: UserEntity,
     itm_uuid: string,
     cmpitm_uuid: string,
     mitm_uuid: string,
+    mitm?: ModelItemEntity,
+    workDetails?: WorkDetailEntity[],
+    workAttachments?: WorkAttachmentEntity[],
 	wrk_createdat: Date,
     wrk_updatedat: Date
 }
