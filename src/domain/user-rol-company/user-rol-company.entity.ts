867c588de608459e5e7cae03d978bb3ba00e156e
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-10-09 09:50:58 -0300

    i add usr property to user-rol-company.entity.ts

--- src/domain/user-rol-company/user-rol-company.entity.ts
+++ src/domain/user-rol-company/user-rol-company.entity.ts
@@ -1,7 +1,10 @@
+import { UserEntity } from "../user/user.entity";
+
 export interface UserRolCompanyEntity {
     cmp_uuid: string,
     usrrolcmp_uuid: string,
     usr_uuid: string,
+    usr?: UserEntity,
     rol_uuid: string,
     usrrolcmp_createdat: Date,
     usrrolcmp_updatedat: Date
