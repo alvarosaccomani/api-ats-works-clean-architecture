fc4842db3c951fc625f479a826cf675b13b2117c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-26 19:46:28 -0300

    i add menu.repository.ts into menu folder into folder domain to project

---
+++ src/domain/menu/menu.repository.ts
@@ -0,0 +1,11 @@
+import { MenuEntity, MenuUpdateData } from "./menu.entity";
+
+export interface MenuRepository {
+    getMenus(mnu_title?: string, field_order?: string, mnu_orderby?: string): Promise<MenuEntity[] | null>;
+    findMenuById(mnu_uuid: string): Promise<MenuEntity | null>;
+    createMenu(menu: MenuEntity): Promise<MenuEntity | null>;
+    updateMenu(mnu_uuid: string, menu: MenuUpdateData): Promise<MenuEntity | null>;
+    deleteMenu(mnu_uuid: string): Promise<MenuEntity | null>;
+    findMenuByTitle(mnu_title: string): Promise<MenuEntity | null>;
+    getMenuItems(): Promise<MenuEntity[] | null>;
+}
